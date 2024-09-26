e8706da6412370266eea6b2235a9233e5efc7dc3
summydeveloper <adegbiteadekola54@gmail.com>
2024-09-26 17:47:26 +0100

    commit

--- app.py
+++ app.py
@@ -1,121 +1,3 @@
-# import streamlit as st 
-# from tqdm import tqdm
-# import pandas as pd
-# from typing import Optional, List
-# import datasets
-# from langchain.docstore.document import Document as LangchainDocument
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from transformers import AutoTokenizer
-# from langchain_community.vectorstores import FAISS
-# from langchain_huggingface import HuggingFaceEmbeddings
-# from langchain_community.vectorstores.utils import DistanceStrategy
-
-# class CourseChatbot:
-#     def __init__(self):
-#         pd.set_option("display.max_colwidth", None)
-#         self.dataset = datasets.load_dataset("summydev/lecturersdata", split="train")
-#         self.documents = self.create_documents()
-#         self.embedding_model = self.initialize_embeddings()
-#         self.vector_database = self.create_vector_database()
-      
-#     def create_documents(self) -> List[LangchainDocument]:
-        
- 
-#     # Your document creation code here
-
-#         return [
-#             LangchainDocument(
-#                 page_content=doc["description"],
-#                 metadata={
-#                     "source": doc["source"],
-#                     "instructor_name": doc["instructorname"],
-#                     "course_title": doc["coursetitle"],
-#                     "rating": doc["rating"],
-#                 }
-#             ) for doc in tqdm(self.dataset)
-#         ]
-
-#     def initialize_embeddings(self) -> HuggingFaceEmbeddings:
-#         embedding_model_name = "thenlper/gte-small"
-#         return HuggingFaceEmbeddings(
-#             model_name=embedding_model_name,
-#             multi_process=True,
-#             model_kwargs={"device": "cpu"},
-#             encode_kwargs={"normalize_embeddings": True},
-#         )
-    
-#     def create_vector_database(self) -> FAISS:
-#         chunk_size = 512
-#         processed_docs = self.split_documents(chunk_size)
-#         return FAISS.from_documents(processed_docs, self.embedding_model, distance_strategy=DistanceStrategy.COSINE)
-
-#     def split_documents(self, chunk_size: int) -> List[LangchainDocument]:
-#         tokenizer = AutoTokenizer.from_pretrained("thenlper/gte-small")
-#         text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-#             tokenizer,
-#             chunk_size=chunk_size,
-#             chunk_overlap=int(chunk_size / 10),
-#             add_start_index=True,
-#             strip_whitespace=True,
-#             separators=[
-#                 "\n#{1,6} ", "\n", "\n\\*\\*\\*+\n", "\n---+\n", "\n___+\n", "\n\n", "\n", " ", ""
-#             ],
-#         )
-        
-#         docs_processed = []
-#         for doc in self.documents:
-#             docs_processed += text_splitter.split_documents([doc])
-
-#         # Remove duplicates
-#         unique_texts = {}
-#         docs_processed_unique = []
-#         for doc in docs_processed:
-#             if doc.page_content not in unique_texts:
-#                 unique_texts[doc.page_content] = True
-#                 docs_processed_unique.append(doc)
-
-#         return docs_processed_unique
-
-#     def respond_to_query(self, user_query: str):
-#         query_vector = self.embedding_model.embed_query(user_query)
-#         retrieved_docs = self.vector_database.similarity_search(query= query_vector, k=5)
-        
-#         responses = []
-#         for i, doc in enumerate(retrieved_docs):
-#             responses.append({
-#                 "rank": i + 1,
-#                 "description": doc.page_content,
-#                 "instructor": doc.metadata.get("instructor_name", "N/A"),
-#                 "course_title": doc.metadata.get("course_title", "N/A"),
-#                 "source": doc.metadata.get("source", "N/A"),
-#             })
-#         return responses
-
-# # Streamlit UI
-# def main():
-#     st.title("Rate My Prof")
-    
-#     chatbot = CourseChatbot()
-    
-#     user_input = st.text_input("What would you like to know about the courses?")
-    
-#     if st.button("Get Info"):
-#         if user_input:
-#             responses = chatbot.respond_to_query(user_input)
-#             for response in responses:
-#                 st.subheader(f"Search Results{response['rank']}")
-#                 st.write(response['description'])
-#                 st.write("**Instructor:**", response['instructor'])
-#                 st.write("**Course Title:**", response['course_title'])
-#                 st.write("**Source:**", response['source'])
-#                 st.markdown("---")
-#         else:
-#             st.warning("Please enter a query.")
-
-# if __name__ == '__main__':
-#     main()
-
-
 # from flask import Flask, request, jsonify, render_template
 # from tqdm import tqdm
 # import pandas as pd
@@ -329,7 +211,6 @@
 # if __name__ == '__main__':
 #     main()
 
-
 import streamlit as st
 from tqdm import tqdm
 import pandas as pd
@@ -343,7 +224,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores.utils import DistanceStrategy
 
 # Load knowledge base
-@st.cache_resource(show_spinner=False)
+@st.cache_data(show_spinner=False)
 def load_knowledge_base():
     pd.set_option("display.max_colwidth", None)
     ds = datasets.load_dataset("summydev/lecturersdata", split="train")
@@ -395,7 +276,7 @@ def initialize_chatbot():
 
     embedding_model = HuggingFaceEmbeddings(
         model_name=EMBEDDING_MODEL_NAME,
-        multi_process=True,
+        multi_process=False,  # Set to False for testing
         model_kwargs={"device": "cpu"},
         encode_kwargs={"normalize_embeddings": True},
     )
